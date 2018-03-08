2fd272703aec437c7ce5aead4c748b8fc035f7c5
Ben Verbeken <ben@seats.io>
2018-03-08 11:28:39 +0100

    refactoring

--- seatsio/client.py
+++ seatsio/client.py
@@ -1,7 +1,7 @@
 from bunch import bunchify
 
 from seatsio.domain import Chart
-from seatsio.httpClient import POST, GET
+from seatsio.httpClient import POST, GET, HttpClient
 
 
 class Client:
@@ -9,22 +9,22 @@ class Client:
     def __init__(self, secret_key, base_url="https://api.seats.io"):
         self.secretKey = secret_key
         self.baseUrl = base_url
-        self.charts = Charts(self.secretKey, self.baseUrl)
+        self.httpClient = HttpClient(base_url, secret_key)
+        self.charts = Charts(self.httpClient)
 
 
 class Charts:
 
-    def __init__(self, secret_key, base_url):
-        self.secretKey = secret_key
-        self.baseUrl = base_url
+    def __init__(self, http_client):
+        self.httpClient = http_client
 
     def retrieve(self, chart_key):
-        url = self.baseUrl + "/charts/" + chart_key
-        response = GET(url).basic_auth(self.secretKey, '').execute()
+        url = "/charts/" + chart_key
+        response = self.httpClient.get(url).execute()
         return bunchify(response.body)
 
     def create(self, name=None, venue_type=None, categories=None):
-        url = self.baseUrl + "/charts"
+        url = "/charts"
         body = {}
         if name:
             body['name'] = name
@@ -32,15 +32,15 @@ class Charts:
             body['venueType'] = venue_type
         if categories:
             body['categories'] = categories
-        response = POST(url).basic_auth(self.secretKey, '').body(body).execute()
+        response = self.httpClient.post(url).body(body).execute()
         return Chart(response.body)
 
     def retrieve_published_version(self, key):
-        url = self.baseUrl + "/charts/" + key + "/version/published"
-        response = GET(url).basic_auth(self.secretKey, '').execute()
+        url = "/charts/" + key + "/version/published"
+        response = self.httpClient.get(url).execute()
         return bunchify(response.body)
 
     def add_tag(self, key, tag):
-        url = self.baseUrl + "/charts/" + key + "/tags/" + tag
-        response = POST(url).basic_auth(self.secretKey, '').execute()
+        url = "/charts/" + key + "/tags/" + tag
+        response = self.httpClient.post(url).execute()
         return response
--- seatsio/httpClient.py
+++ seatsio/httpClient.py
@@ -4,13 +4,25 @@ import unirest
 from seatsio.exceptions import SeatsioException
 
 
+class HttpClient:
+    def __init__(self, base_url, secret_key):
+        self.baseUrl = base_url
+        self.secretKey = secret_key
+
+    def get(self, url):
+        return GET(self.baseUrl + url).auth(self.secretKey, '')
+
+    def post(self, url):
+        return POST(self.baseUrl + url).auth(self.secretKey, '')
+
+
 class GET:
     def __init__(self, url):
         self.httpMethod = "GET"
         self.url = url
 
-    def basic_auth(self, username, password):
-        self.auth = (username, password)
+    def auth(self, username, password):
+        self.credentials = (username, password)
         return self
 
     def execute(self):
@@ -22,7 +34,7 @@ class GET:
 
     def __try_execute(self):
         try:
-            return unirest.get(self.url, auth=self.auth)
+            return unirest.get(self.url, auth=self.credentials)
         except Exception as cause:
             raise SeatsioException(self, cause=cause)
 
@@ -33,8 +45,8 @@ class POST:
         self.url = url
         self.bodyObject = None
 
-    def basic_auth(self, username, password):
-        self.auth = (username, password)
+    def auth(self, username, password):
+        self.credentials = (username, password)
         return self
 
     def body(self, body):
@@ -54,14 +66,14 @@ class POST:
                 json = jsonpickle.encode(self.bodyObject, unpicklable=False)
                 return unirest.post(
                     url=self.url,
-                    auth=self.auth,
+                    auth=self.credentials,
                     headers={"Accept": "application/json"},
                     params=json
                 )
             else:
                 return unirest.post(
                     url=self.url,
-                    auth=self.auth
+                    auth=self.credentials
                 )
         except Exception as cause:
             raise SeatsioException(self, cause=cause)
