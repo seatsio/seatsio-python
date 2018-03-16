94fb9ee32aee7a88d87ada7fa521262de9354cbe
Ben Verbeken <ben@seats.io>
2018-03-16 10:23:33 +0100

    refactoring: moved charts code to separate package

---
+++ seatsio/charts/chartsClient.py
@@ -0,0 +1,96 @@
+from seatsio import Chart, bunchify
+from seatsio.pagination.lister import Lister
+from seatsio.pagination.pageFetcher import PageFetcher
+
+
+class ChartsClient:
+
+    def __init__(self, http_client):
+        self.http_client = http_client
+
+    def retrieve(self, chart_key):
+        return self.http_client.url("/charts/{key}", key=chart_key).get_as(Chart)
+
+    def retrieve_with_events(self, chart_key):
+        return self.http_client.url("/charts/{key}?expand=events", key=chart_key).get_as(Chart)
+
+    def create(self, name=None, venue_type=None, categories=None):
+        request = ChartRequest(name, venue_type, categories)
+        response = self.http_client.url("/charts").post(request)
+        return Chart(response.body)
+
+    # TODO return something else than a bunch
+    def retrieve_published_version(self, key):
+        response = self.http_client.url("/charts/{key}/version/published", key=key).get()
+        return bunchify(response.body)
+
+    # TODO return something else than a bunch
+    def retrieve_draft_version(self, key):
+        response = self.http_client.url("/charts/{key}/version/draft", key=key).get()
+        return bunchify(response.body)
+
+    def retrieve_draft_version_thumbnail(self, key):
+        return self.http_client.url("/charts/{key}/version/draft/thumbnail", key=key).get().raw_body
+
+    def retrieve_published_version_thumbnail(self, key):
+        return self.http_client.url("/charts/{key}/version/published/thumbnail", key=key).get().raw_body
+
+    def copy(self, key):
+        return self.http_client \
+            .url("/charts/{key}/version/published/actions/copy", key=key) \
+            .post_empty_and_return(Chart)
+
+    def copy_to_subaccount(self, chart_key, subaccount_id):
+        return self.http_client \
+            .url("/charts/{key}/version/published/actions/copy-to/{subaccountId}",
+                 key=chart_key,
+                 subaccountId=subaccount_id) \
+            .post_empty_and_return(Chart)
+
+    def copy_draft_version(self, key):
+        return self.http_client \
+            .url("/charts/{key}/version/draft/actions/copy", key=key) \
+            .post_empty_and_return(Chart)
+
+    def discard_draft_version(self, key):
+        self.http_client.url("/charts/{key}/version/draft/actions/discard", key=key).post()
+
+    def update(self, key, new_name=None, categories=None):
+        request = ChartRequest(name=new_name, categories=categories)
+        self.http_client.url("/charts/{key}", key=key).post(request)
+
+    def move_to_archive(self, chart_key):
+        self.http_client.url("/charts/{key}/actions/move-to-archive", key=chart_key).post()
+
+    def move_out_of_archive(self, chart_key):
+        self.http_client.url("/charts/{key}/actions/move-out-of-archive", key=chart_key).post()
+
+    def publish_draft_version(self, chart_key):
+        self.http_client.url("/charts/{key}/version/draft/actions/publish", key=chart_key).post()
+
+    def list_all_tags(self):
+        response = self.http_client.url("/charts/tags").get()
+        return response.body["tags"]
+
+    def add_tag(self, key, tag):
+        return self.http_client.url("/charts/{key}/tags/{tag}", key=key, tag=tag).post()
+
+    def remove_tag(self, key, tag):
+        self.http_client.url("/charts/{key}/tags/{tag}", key=key, tag=tag).delete()
+
+    def list(self):
+        return Lister(PageFetcher(Chart, self.http_client, "/charts"))
+
+    def archive(self):
+        return Lister(PageFetcher(Chart, self.http_client, "/charts/archive"))
+
+
+class ChartRequest:
+
+    def __init__(self, name=None, venue_type=None, categories=None):
+        if name:
+            self.name = name
+        if venue_type:
+            self.venueType = venue_type
+        if categories:
+            self.categories = categories
--- seatsio/client.py
+++ seatsio/client.py
@@ -1,4 +1,4 @@
-from seatsio.charts.chartRequest import ChartRequest
+from seatsio.charts.chartsClient import ChartsClient
 from seatsio.domain import *
 from seatsio.httpClient import HttpClient
 from seatsio.pagination.lister import Lister
@@ -10,92 +10,10 @@ class Client:
     def __init__(self, secret_key, base_url="https://api.seats.io"):
         self.base_url = base_url
         self.http_client = HttpClient(base_url, secret_key)
-        self.charts = Charts(self.http_client)
+        self.charts = ChartsClient(self.http_client)
         self.events = Events(self.http_client)
         self.subaccounts = Subaccounts(self.http_client)
         self.hold_tokens = HoldTokens(self.http_client)
-
-
-class Charts:
-
-    def __init__(self, http_client):
-        self.http_client = http_client
-
-    def retrieve(self, chart_key):
-        return self.http_client.url("/charts/{key}", key=chart_key).get_as(Chart)
-
-    def retrieve_with_events(self, chart_key):
-        return self.http_client.url("/charts/{key}?expand=events", key=chart_key).get_as(Chart)
-
-    def create(self, name=None, venue_type=None, categories=None):
-        request = ChartRequest(name, venue_type, categories)
-        response = self.http_client.url("/charts").post(request)
-        return Chart(response.body)
-
-    # TODO return something else than a bunch
-    def retrieve_published_version(self, key):
-        response = self.http_client.url("/charts/{key}/version/published", key=key).get()
-        return bunchify(response.body)
-
-    # TODO return something else than a bunch
-    def retrieve_draft_version(self, key):
-        response = self.http_client.url("/charts/{key}/version/draft", key=key).get()
-        return bunchify(response.body)
-
-    def retrieve_draft_version_thumbnail(self, key):
-        return self.http_client.url("/charts/{key}/version/draft/thumbnail", key=key).get().raw_body
-
-    def retrieve_published_version_thumbnail(self, key):
-        return self.http_client.url("/charts/{key}/version/published/thumbnail", key=key).get().raw_body
-
-    def copy(self, key):
-        return self.http_client \
-            .url("/charts/{key}/version/published/actions/copy", key=key) \
-            .post_empty_and_return(Chart)
-
-    def copy_to_subaccount(self, chart_key, subaccount_id):
-        return self.http_client \
-            .url("/charts/{key}/version/published/actions/copy-to/{subaccountId}",
-                 key=chart_key,
-                 subaccountId=subaccount_id) \
-            .post_empty_and_return(Chart)
-
-    def copy_draft_version(self, key):
-        return self.http_client \
-            .url("/charts/{key}/version/draft/actions/copy", key=key) \
-            .post_empty_and_return(Chart)
-
-    def discard_draft_version(self, key):
-        self.http_client.url("/charts/{key}/version/draft/actions/discard", key=key).post()
-
-    def update(self, key, new_name=None, categories=None):
-        request = ChartRequest(name=new_name, categories=categories)
-        self.http_client.url("/charts/{key}", key=key).post(request)
-
-    def move_to_archive(self, chart_key):
-        self.http_client.url("/charts/{key}/actions/move-to-archive", key=chart_key).post()
-
-    def move_out_of_archive(self, chart_key):
-        self.http_client.url("/charts/{key}/actions/move-out-of-archive", key=chart_key).post()
-
-    def publish_draft_version(self, chart_key):
-        self.http_client.url("/charts/{key}/version/draft/actions/publish", key=chart_key).post()
-
-    def list_all_tags(self):
-        response = self.http_client.url("/charts/tags").get()
-        return response.body["tags"]
-
-    def add_tag(self, key, tag):
-        return self.http_client.url("/charts/{key}/tags/{tag}", key=key, tag=tag).post()
-
-    def remove_tag(self, key, tag):
-        self.http_client.url("/charts/{key}/tags/{tag}", key=key, tag=tag).delete()
-
-    def list(self):
-        return Lister(PageFetcher(Chart, self.http_client, "/charts"))
-
-    def archive(self):
-        return Lister(PageFetcher(Chart, self.http_client, "/charts/archive"))
 
 
 class Events:
