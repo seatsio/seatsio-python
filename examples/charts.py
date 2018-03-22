bbf1898d40209af59408e75ca50fea7f89df2593
Ben Verbeken <ben@seats.io>
2018-03-22 14:38:57 +0100

    changed examples to reflect simpler API

--- examples/charts.py
+++ examples/charts.py
@@ -6,8 +6,11 @@ client = seatsio.Client(secret_key="my-secret-key")
 # create a chart
 chart = client.charts.create()
 
-# find all my charts - returns a paged iterator
-my_charts = client.charts.list().all()
+# find my charts
+all_my_charts = client.charts.list()
+tagged_charts = client.charts.list(tag="WestEnd")
+charts_with_name = client.charts.list(chart_filter="Theatre")
+charts_with_events = client.charts.list(expand_events=True)
 
 # retrieve a single chart
 a_single_chart = client.charts.retrieve("chart_key")
