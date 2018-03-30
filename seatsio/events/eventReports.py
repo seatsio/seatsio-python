e4b032b125beef58105b35d4d7dd4713737d1b3d
mroloux <matti@seats.io>
2018-03-30 11:47:30 +0200

    Added event report summary calls

--- seatsio/events/eventReports.py
+++ seatsio/events/eventReports.py
@@ -8,11 +8,20 @@ class EventReports:
     def by_status(self, event_key, status=None):
         return self.__fetch_report("byStatus", event_key, status)
 
+    def summary_by_status(self, event_key):
+        return self.__fetch_summary_report("byStatus", event_key)
+
     def by_category_label(self, event_key, category_label=None):
         return self.__fetch_report("byCategoryLabel", event_key, category_label)
 
+    def summary_by_category_label(self, event_key):
+        return self.__fetch_summary_report("byCategoryLabel", event_key)
+
     def by_category_key(self, event_key, category_key=None):
         return self.__fetch_report("byCategoryKey", event_key, category_key)
+
+    def summary_by_category_key(self, event_key):
+        return self.__fetch_summary_report("byCategoryKey", event_key)
 
     def by_label(self, event_key, label=None):
         return self.__fetch_report("byLabel", event_key, label)
@@ -22,6 +31,9 @@ class EventReports:
 
     def by_section(self, event_key, section=None):
         return self.__fetch_report("bySection", event_key, section)
+
+    def summary_by_section(self, event_key):
+        return self.__fetch_summary_report("bySection", event_key)
 
     def __fetch_report(self, report_type, event_key, report_filter=None):
         if report_filter:
@@ -35,3 +47,7 @@ class EventReports:
             url = "/reports/events/{key}/{reportType}"
             body = self.http_client.url(url, key=event_key, reportType=report_type).get()
             return EventReport(body)
+
+    def __fetch_summary_report(self, report_type, event_key):
+        url = "/reports/events/{key}/{reportType}/summary"
+        return self.http_client.url(url, key=event_key, reportType=report_type).get()
