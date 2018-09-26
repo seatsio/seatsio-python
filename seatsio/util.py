51c18fd5e9a93f1fa8370b91de34b747c1acd8c4
mroloux <matti@seats.io>
2018-09-26 21:01:25 +0200

    Fallback to other date format if microseconds are not present

--- seatsio/util.py
+++ seatsio/util.py
@@ -5,4 +5,7 @@ def parse_date(date_as_str):
     if date_as_str is None:
         return None
     else:
-        return datetime.strptime(date_as_str, "%Y-%m-%dT%H:%M:%S.%fZ")
+        try:
+            return datetime.strptime(date_as_str, "%Y-%m-%dT%H:%M:%S.%fZ")
+        except ValueError:
+            return datetime.strptime(date_as_str, "%Y-%m-%dT%H:%M:%SZ")
