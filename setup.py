a53a28fff561cb57d6904d89c8ea4631c2189ab0
mroloux <matti.roloux@gmail.com>
2019-02-05 13:04:45 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name='seatsio',
-    version='v34',
+    version='v35',
     description='The official Seats.io Python client library',
     long_description=readme,
     author='The seats.io dev team',
