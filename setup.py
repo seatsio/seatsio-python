c59df5ba8f6d2021d5a90cc024156840c33dee8f
Nahuel Chaves <nahuel@seats.io>
2019-03-27 15:25:23 -0300

    *  add Chart Report API endpoints (by_categor_Label and by_category_key)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name='seatsio',
-    version='v36',
+    version='v37',
     description='The official Seats.io Python client library',
     long_description=readme,
     author='The seats.io dev team',
