75a0e25b843dae24b33df19974342179403f2da8
mroloux <matti@seats.io>
2019-04-15 21:06:26 +0200

    Fixed tests

--- tests/events/eventReportsSummaryTest.py
+++ tests/events/eventReportsSummaryTest.py
@@ -18,12 +18,12 @@ class EventReportsSummaryTest(SeatsioClientTest):
         assert_that(report.get("booked").get("byCategoryKey").get("9")).is_equal_to(1)
         assert_that(report.get("booked").get("byCategoryLabel").get("Cat1")).is_equal_to(1)
 
-        assert_that(report.get("free").get("count")).is_equal_to(33)
-        assert_that(report.get("free").get("bySection").get("NO_SECTION")).is_equal_to(33)
-        assert_that(report.get("free").get("byCategoryKey").get("9")).is_equal_to(16)
-        assert_that(report.get("free").get("byCategoryKey").get("10")).is_equal_to(17)
-        assert_that(report.get("free").get("byCategoryLabel").get("Cat1")).is_equal_to(16)
-        assert_that(report.get("free").get("byCategoryLabel").get("Cat2")).is_equal_to(17)
+        assert_that(report.get("free").get("count")).is_equal_to(231)
+        assert_that(report.get("free").get("bySection").get("NO_SECTION")).is_equal_to(231)
+        assert_that(report.get("free").get("byCategoryKey").get("9")).is_equal_to(115)
+        assert_that(report.get("free").get("byCategoryKey").get("10")).is_equal_to(116)
+        assert_that(report.get("free").get("byCategoryLabel").get("Cat1")).is_equal_to(115)
+        assert_that(report.get("free").get("byCategoryLabel").get("Cat2")).is_equal_to(116)
 
     def test_summaryByCategoryKey(self):
         chart_key = self.create_test_chart()
@@ -33,14 +33,14 @@ class EventReportsSummaryTest(SeatsioClientTest):
 
         report = self.client.events.reports.summary_by_category_key(event.key)
 
-        assert_that(report.get("9").get("count")).is_equal_to(17)
-        assert_that(report.get("9").get("bySection").get("NO_SECTION")).is_equal_to(17)
+        assert_that(report.get("9").get("count")).is_equal_to(116)
+        assert_that(report.get("9").get("bySection").get("NO_SECTION")).is_equal_to(116)
         assert_that(report.get("9").get("byStatus").get("booked")).is_equal_to(1)
-        assert_that(report.get("9").get("byStatus").get("free")).is_equal_to(16)
+        assert_that(report.get("9").get("byStatus").get("free")).is_equal_to(115)
 
-        assert_that(report.get("10").get("count")).is_equal_to(17)
-        assert_that(report.get("10").get("bySection").get("NO_SECTION")).is_equal_to(17)
-        assert_that(report.get("10").get("byStatus").get("free")).is_equal_to(17)
+        assert_that(report.get("10").get("count")).is_equal_to(116)
+        assert_that(report.get("10").get("bySection").get("NO_SECTION")).is_equal_to(116)
+        assert_that(report.get("10").get("byStatus").get("free")).is_equal_to(116)
 
     def test_summaryByCategoryLabel(self):
         chart_key = self.create_test_chart()
@@ -50,14 +50,14 @@ class EventReportsSummaryTest(SeatsioClientTest):
 
         report = self.client.events.reports.summary_by_category_label(event.key)
 
-        assert_that(report.get("Cat1").get("count")).is_equal_to(17)
-        assert_that(report.get("Cat1").get("bySection").get("NO_SECTION")).is_equal_to(17)
+        assert_that(report.get("Cat1").get("count")).is_equal_to(116)
+        assert_that(report.get("Cat1").get("bySection").get("NO_SECTION")).is_equal_to(116)
         assert_that(report.get("Cat1").get("byStatus").get("booked")).is_equal_to(1)
-        assert_that(report.get("Cat1").get("byStatus").get("free")).is_equal_to(16)
+        assert_that(report.get("Cat1").get("byStatus").get("free")).is_equal_to(115)
 
-        assert_that(report.get("Cat2").get("count")).is_equal_to(17)
-        assert_that(report.get("Cat2").get("bySection").get("NO_SECTION")).is_equal_to(17)
-        assert_that(report.get("Cat2").get("byStatus").get("free")).is_equal_to(17)
+        assert_that(report.get("Cat2").get("count")).is_equal_to(116)
+        assert_that(report.get("Cat2").get("bySection").get("NO_SECTION")).is_equal_to(116)
+        assert_that(report.get("Cat2").get("byStatus").get("free")).is_equal_to(116)
 
     def test_summaryBySection(self):
         chart_key = self.create_test_chart()
@@ -67,10 +67,10 @@ class EventReportsSummaryTest(SeatsioClientTest):
 
         report = self.client.events.reports.summary_by_section(event.key)
 
-        assert_that(report.get("NO_SECTION").get("count")).is_equal_to(34)
+        assert_that(report.get("NO_SECTION").get("count")).is_equal_to(232)
         assert_that(report.get("NO_SECTION").get("byStatus").get("booked")).is_equal_to(1)
-        assert_that(report.get("NO_SECTION").get("byStatus").get("free")).is_equal_to(33)
-        assert_that(report.get("NO_SECTION").get("byCategoryKey").get("9")).is_equal_to(17)
-        assert_that(report.get("NO_SECTION").get("byCategoryKey").get("10")).is_equal_to(17)
-        assert_that(report.get("NO_SECTION").get("byCategoryLabel").get("Cat1")).is_equal_to(17)
-        assert_that(report.get("NO_SECTION").get("byCategoryLabel").get("Cat2")).is_equal_to(17)
+        assert_that(report.get("NO_SECTION").get("byStatus").get("free")).is_equal_to(231)
+        assert_that(report.get("NO_SECTION").get("byCategoryKey").get("9")).is_equal_to(116)
+        assert_that(report.get("NO_SECTION").get("byCategoryKey").get("10")).is_equal_to(116)
+        assert_that(report.get("NO_SECTION").get("byCategoryLabel").get("Cat1")).is_equal_to(116)
+        assert_that(report.get("NO_SECTION").get("byCategoryLabel").get("Cat2")).is_equal_to(116)
