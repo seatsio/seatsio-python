f3769441a55a7e946b68c5f240663a0227214bcb
Nahuel Chaves <nahuel@seats.io>
2019-04-17 09:22:05 -0300

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name='seatsio',
-    version='v38',
+    version='v39',
     description='The official Seats.io Python client library',
     long_description=readme,
     author='The seats.io dev team',
