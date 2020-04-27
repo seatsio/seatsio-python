f70f98085a2b895676ec6ed54a5fcb053bc6001b
mroloux <matti@seats.io>
2020-04-27 10:25:06 +0200

    Attempt to fix build

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     install_requires=[
         "requests>=2.20",
         "munch>=2.3",
-        "jsonpickle>=1.3",
+        "jsonpickle>=1.0, <1.4",
         "future>=0.18",
         "six>=1.10"
     ],
