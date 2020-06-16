432b6fb36f7709a9f532ab305db469ea58145ba2
Ben Verbeken <ben@seats.io>
2020-06-16 16:43:07 +0200

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name='seatsio',
-    version='v51.1.0',
+    version='v51.2.0',
     description='The official Seats.io Python client library',
     author='The seats.io dev team',
     author_email='hello@seats.io',
