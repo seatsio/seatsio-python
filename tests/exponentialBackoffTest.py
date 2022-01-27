618d8b2e35ae04800a51e7afc34c8a652ea6b0ef
Matti Roloux <matti@seats.io>
2022-01-27 16:17:31 +0100

    mockbin isn't working anymore

--- tests/exponentialBackoffTest.py
+++ tests/exponentialBackoffTest.py
@@ -11,55 +11,55 @@ class ExponentialBackoffTest(SeatsioClientTest):
     def test_aborts_eventually_if_server_keeps_returning_429_get(self):
         start = time.time()
         try:
-            client = HttpClient("https://mockbin.org", "aSecretKey", None, 5)
-            client.url("/bin/0381d6f4-0155-4b8c-937b-73d3d88b2a3f").get()
+            client = HttpClient("https://httpbin.org", "aSecretKey", None, 5)
+            client.url("/status/429").get()
             raise Exception("Should have failed")
         except RateLimitExceededException as e:
-            assert_that(e.errors[0]['code']).is_equal_to("RATE_LIMIT_EXCEEDED")
+            assert_that(e.message).is_equal_to("Error while executing GET https://httpbin.org/status/429")
             wait_time = int(time.time() - start)
             assert_that(wait_time).is_between(10, 20)
 
     def test_aborts_eventually_if_server_keeps_returning_429_post(self):
         start = time.time()
         try:
-            client = HttpClient("https://mockbin.org", "aSecretKey", None, 5)
-            client.url("/bin/0381d6f4-0155-4b8c-937b-73d3d88b2a3f").post()
+            client = HttpClient("https://httpbin.org", "aSecretKey", None, 5)
+            client.url("/status/429").post()
             raise Exception("Should have failed")
         except RateLimitExceededException as e:
-            assert_that(e.errors[0]['code']).is_equal_to("RATE_LIMIT_EXCEEDED")
+            assert_that(e.message).is_equal_to("Error while executing POST https://httpbin.org/status/429")
             wait_time = int(time.time() - start)
             assert_that(wait_time).is_between(10, 20)
 
     def test_aborts_eventually_if_server_keeps_returning_429_delete(self):
         start = time.time()
         try:
-            client = HttpClient("https://mockbin.org", "aSecretKey", None, 5)
-            client.url("/bin/0381d6f4-0155-4b8c-937b-73d3d88b2a3f").delete()
+            client = HttpClient("https://httpbin.org", "aSecretKey", None, 5)
+            client.url("/status/429").delete()
             raise Exception("Should have failed")
         except RateLimitExceededException as e:
-            assert_that(e.errors[0]['code']).is_equal_to("RATE_LIMIT_EXCEEDED")
+            assert_that(e.message).is_equal_to("Error while executing DELETE https://httpbin.org/status/429")
             wait_time = int(time.time() - start)
             assert_that(wait_time).is_between(10, 20)
 
     def test_aborts_directly_if_server_returns_other_error_than_429(self):
         start = time.time()
         try:
-            client = HttpClient("https://mockbin.org", "aSecretKey", None, 5)
-            client.url("/bin/1eea3aab-2bb2-4f92-99c2-50d942fb6294").get()
+            client = HttpClient("https://httpbin.org", "aSecretKey", None, 5)
+            client.url("/status/400").get()
             raise Exception("Should have failed")
         except SeatsioException as e:
-            assert_that(e.message).is_equal_to("Error while executing GET https://mockbin.org/bin/1eea3aab-2bb2-4f92-99c2-50d942fb6294")
+            assert_that(e.message).is_equal_to("Error while executing GET https://httpbin.org/status/400")
             wait_time = int(time.time() - start)
             assert_that(wait_time).is_between(0, 2)
 
     def test_aborts_directly_if_server_returns_429_but_max_retries_0(self):
         start = time.time()
         try:
-            client = HttpClient("https://mockbin.org", "aSecretKey", None, 0)
-            client.url("/bin/0381d6f4-0155-4b8c-937b-73d3d88b2a3f").get()
+            client = HttpClient("https://httpbin.org", "aSecretKey", None, 0)
+            client.url("/status/429").get()
             raise Exception("Should have failed")
         except RateLimitExceededException as e:
-            assert_that(e.errors[0]['code']).is_equal_to("RATE_LIMIT_EXCEEDED")
+            assert_that(e.message).is_equal_to("Error while executing GET https://httpbin.org/status/429")
             wait_time = int(time.time() - start)
             assert_that(wait_time).is_between(0, 2)
 
