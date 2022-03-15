4e724740a996258e1268ec543aa3882f8ee4a71d
Ben Verbeken <ben@seats.io>
2022-03-15 12:23:30 +0100

    Call that creates events in season returns events instead of whole season

--- tests/seasons/createEventsInSeasonTest.py
+++ tests/seasons/createEventsInSeasonTest.py
@@ -8,16 +8,14 @@ class CreateEventsInSeasonTest(SeatsioClientTest):
         chart = self.client.charts.create()
         season = self.client.seasons.create(chart.key)
 
-        updated_partial_season = self.client.seasons.create_events(season.key, event_keys=["event1", "event2"])
+        events = self.client.seasons.create_events(season.key, event_keys=["event1", "event2"])
 
-        assert_that(updated_partial_season.events).extracting("key").contains_exactly("event1", "event2")
-        assert_that(updated_partial_season.events[0].is_event_in_season).is_true()
-        assert_that(updated_partial_season.events[0].top_level_season_key).is_equal_to(season.key)
+        assert_that(events).extracting("key").contains_exactly("event2", "event1")
 
     def test_number_of_events(self):
         chart = self.client.charts.create()
         season = self.client.seasons.create(chart.key)
 
-        updated_partial_season = self.client.seasons.create_events(season.key, number_of_events=2)
+        events = self.client.seasons.create_events(season.key, number_of_events=2)
 
-        assert_that(updated_partial_season.events).has_size(2)
+        assert_that(events).has_size(2)
