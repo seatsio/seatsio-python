f8efff2772db97a78810fa7ff8031cca240b627f
Matti Roloux <matti@seats.io>
2022-05-19 16:40:13 +0200

    Version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name='seatsio',
-    version='v66.2.0',
+    version='v67.0.0',
     description='The official Seats.io Python client library',
     author='The seats.io dev team',
     author_email='hello@seats.io',
