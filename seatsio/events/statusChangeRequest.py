eb0e1b780d729d1cd00025c7e08aae346fe8ea89
mroloux <matti@seats.io>
2024-11-22 09:43:48 +0100

    Support overriding or using the season status in batch (#97)

--- seatsio/events/statusChangeRequest.py
+++ seatsio/events/statusChangeRequest.py
@@ -1,6 +1,8 @@
 class StatusChangeRequest:
     TYPE_CHANGE_STATUS_TO = "CHANGE_STATUS_TO"
     RELEASE = "RELEASE"
+    OVERRIDE_SEASON_STATUS = "OVERRIDE_SEASON_STATUS"
+    USE_SEASON_STATUS = "USE_SEASON_STATUS"
 
     def __init__(self, event_key, object_or_objects, status=None, hold_token=None, order_id=None, keep_extra_data=None, ignore_channels=None, channel_keys=None, allowed_previous_statuses=None, rejected_previous_statuses=None, type=None):
             self.type = type
