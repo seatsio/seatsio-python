eaa126bc566cd6e61269c1283dcdb6e2790b5d97
Steve Chaloner <steve@seats.io>
2025-06-17 13:54:20 +0200

    Removed unnecessary test

--- tests/workspaces/testDeleteWorkspace.py
+++ tests/workspaces/testDeleteWorkspace.py
@@ -16,12 +16,3 @@ class DeactivateWorkspaceTest(SeatsioClientTest):
             self.fail("expected an exception")
         except SeatsioException as e:
             assert_that(e.message).contains("No workspace found with public key '" + workspace.key + "'")
-
-    def test_delete_active_workspace(self):
-        workspace = self.client.workspaces.create("a ws")
-
-        try:
-            self.client.workspaces.delete(workspace.key)
-            self.fail("expected an exception")
-        except SeatsioException as e:
-            assert_that(e.message).contains("Cannot delete active workspace, please deactivate it first")
