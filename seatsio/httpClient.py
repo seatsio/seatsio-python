36cbb17f4ab6d8fa557870f10192c30fa3768311
Scott Divelbiss <scottdivelbiss@gmail.com>
2026-01-21 09:47:31 +0100

    update(httpClient): Add 10 second timeout (#133)
    * update(httpClient): Add 10 second timeout
    
    * test

--- seatsio/httpClient.py
+++ seatsio/httpClient.py
@@ -12,25 +12,27 @@ def handle_error(request, response):
 
 
 class HttpClient:
-    def __init__(self, base_url, secret_key, workspace_key, max_retries):
+    def __init__(self, base_url, secret_key, workspace_key, max_retries, timeout=10):
         self.base_url = base_url
         self.secret_key = secret_key
         self.workspace_key = workspace_key
         self.max_retries = max_retries
+        self.timeout = timeout
 
     def url(self, relative_url, query_params=None, **path_params):
         if query_params is None:
             query_params = {}
         return ApiResource(self.max_retries, self.secret_key, self.workspace_key, self.base_url, relative_url,
-                           query_params, **path_params)
+                           query_params, self.timeout, **path_params)
 
 
 class ApiResource:
-    def __init__(self, max_retries, secret_key, workspace_key, base_url, relative_url, query_params, **path_params):
+    def __init__(self, max_retries, secret_key, workspace_key, base_url, relative_url, query_params, timeout, **path_params):
         self.max_retries = max_retries
         self.url = self.__create_full_url(base_url, relative_url, query_params, **path_params)
         self.secret_key = secret_key
         self.workspace_key = workspace_key
+        self.timeout = timeout
 
     def __create_full_url(self, base_url, relative_url, query_params, **path_params):
         for key in path_params:
@@ -41,38 +43,39 @@ class ApiResource:
         return full_url
 
     def get(self):
-        return GET(self.max_retries, self.url, self.secret_key, self.workspace_key).execute()
+        return GET(self.max_retries, self.url, self.secret_key, self.workspace_key, self.timeout).execute()
 
     def get_raw(self):
-        return GET(self.max_retries, self.url, self.secret_key, self.workspace_key).execute_raw()
+        return GET(self.max_retries, self.url, self.secret_key, self.workspace_key, self.timeout).execute_raw()
 
     def get_as(self, cls):
         return cls(self.get())
 
     def post(self, body=None):
         if body is None:
-            return POST(self.max_retries, self.url, self.secret_key, self.workspace_key).execute()
+            return POST(self.max_retries, self.url, self.secret_key, self.workspace_key, self.timeout).execute()
         else:
-            return POST(self.max_retries, self.url, self.secret_key, self.workspace_key).body(body).execute()
+            return POST(self.max_retries, self.url, self.secret_key, self.workspace_key, self.timeout).body(body).execute()
 
     def post_empty_and_return(self, cls):
         return cls(self.post().json())
 
     def delete(self, body=None):
         if body is None:
-            return DELETE(self.max_retries, self.url, self.secret_key, self.workspace_key).execute()
+            return DELETE(self.max_retries, self.url, self.secret_key, self.workspace_key, self.timeout).execute()
         else:
-            return DELETE(self.max_retries, self.url, self.secret_key, self.workspace_key).body(body).execute()
+            return DELETE(self.max_retries, self.url, self.secret_key, self.workspace_key, self.timeout).body(body).execute()
 
 
 class GET:
 
-    def __init__(self, max_retries, url, secret_key, workspace_key):
+    def __init__(self, max_retries, url, secret_key, workspace_key, timeout):
         self.http_method = "GET"
         self.max_retries = max_retries
         self.url = url
         self.secret_key = secret_key
         self.workspace_key = workspace_key
+        self.timeout = timeout
 
     def execute(self):
         response = retry(self.try_execute, self.max_retries)
@@ -90,19 +93,20 @@ class GET:
 
     def try_execute(self):
         try:
-            return requests.get(self.url, auth=(self.secret_key, ''), headers=(common_headers(self.workspace_key)))
+            return requests.get(self.url, auth=(self.secret_key, ''), headers=(common_headers(self.workspace_key)), timeout=self.timeout)
         except Exception as cause:
             raise SeatsioException(self, cause=cause)
 
 
 class POST:
 
-    def __init__(self, max_retries, url, secret_key, workspace_key):
+    def __init__(self, max_retries, url, secret_key, workspace_key, timeout):
         self.http_method = "POST"
         self.max_retries = max_retries
         self.url = url
         self.secret_key = secret_key
         self.workspace_key = workspace_key
+        self.timeout = timeout
         self.body_object = None
 
     def body(self, body):
@@ -123,7 +127,8 @@ class POST:
                 url=self.url,
                 auth=(self.secret_key, ''),
                 headers=(common_headers(self.workspace_key)),
-                data=json
+                data=json,
+                timeout=self.timeout
             )
         except Exception as cause:
             raise SeatsioException(self, cause=cause)
@@ -131,12 +136,13 @@ class POST:
 
 class DELETE:
 
-    def __init__(self, max_retries, url, secret_key, workspace_key):
+    def __init__(self, max_retries, url, secret_key, workspace_key, timeout):
         self.http_method = "DELETE"
         self.max_retries = max_retries
         self.url = url
         self.secret_key = secret_key
         self.workspace_key = workspace_key
+        self.timeout = timeout
         self.body_object = None
 
     def body(self, body):
@@ -157,7 +163,8 @@ class DELETE:
                 self.url,
                 auth=(self.secret_key, ''),
                 headers=(common_headers(self.workspace_key)),
-                data=json
+                data=json,
+                timeout=self.timeout
             )
         except Exception as cause:
             raise SeatsioException(self, cause=cause)
