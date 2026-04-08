f96139773f50dae7a7c55cc42191b25ddb725c5f
mroloux <matti@seats.io>
2026-04-08 17:02:40 +0200

    Added tests

--- tests/events/testOverrideSeasonObjectStatus.py
+++ tests/events/testOverrideSeasonObjectStatus.py
@@ -17,3 +17,17 @@ class OverrideSeasonObjectStatusTest(SeatsioClientTest):
 
         assert_that(a1_status).is_equal_to(EventObjectInfo.FREE)
         assert_that(a2_status).is_equal_to(EventObjectInfo.FREE)
+
+    def test_season(self):
+        chart_key = self.create_test_chart()
+        season = self.client.seasons.create(chart_key, event_keys=["event1"])
+        self.client.events.book(season.key, ["A-1", "A-2"])
+
+        self.client.events.override_season_object_status("event1", ["A-1", "A-2"], season=season.key)
+
+        a1_status = self.client.events.retrieve_object_info("event1", "A-1").status
+        a2_status = self.client.events.retrieve_object_info("event1", "A-2").status
+
+        assert_that(a1_status).is_equal_to(EventObjectInfo.FREE)
+        assert_that(a2_status).is_equal_to(EventObjectInfo.FREE)
+
