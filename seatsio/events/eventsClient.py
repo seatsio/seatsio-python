8da85eb5de048ca04730bf0c1b2e48d1581256f5
mroloux <matti@seats.io>
2026-09-11 12:55:25 +0200

    Typo

--- seatsio/events/eventsClient.py
+++ seatsio/events/eventsClient.py
@@ -227,8 +227,12 @@ class EventsClient(ListableObjectsClient):
     def mark_everything_as_for_sale(self, key):
         self.http_client.url("/events/{key}/actions/mark-everything-as-for-sale", key=key).post()
 
-    def mark_everything_as_for_not_sale(self, key):
+    def mark_everything_as_not_for_sale(self, key):
         self.http_client.url("/events/{key}/actions/mark-everything-as-not-for-sale", key=key).post()
+
+    # @deprecated
+    def mark_everything_as_for_not_sale(self, key):
+        self.mark_everything_as_not_for_sale(key)
 
     def override_season_object_status(self, key, objects, season=None):
         self.http_client \
--- tests/events/testMarkEverythingAsNotForSale.py
+++ tests/events/testMarkEverythingAsNotForSale.py
@@ -2,13 +2,13 @@ from tests.seatsioClientTest import SeatsioClientTest
 from tests.util.asserts import assert_that
 
 
-class MarkEverythingAsForNotSaleTest(SeatsioClientTest):
+class MarkEverythingAsNotForSaleTest(SeatsioClientTest):
 
     def test(self):
         chart = self.client.charts.create()
         event = self.client.events.create(chart.key)
 
-        self.client.events.mark_everything_as_for_not_sale(event.key)
+        self.client.events.mark_everything_as_not_for_sale(event.key)
 
         retrieved_event = self.client.events.retrieve(event.key)
         assert_that(retrieved_event.for_sale_config).is_not_none()
@@ -16,5 +16,6 @@ class MarkEverythingAsForNotSaleTest(SeatsioClientTest):
         assert_that(retrieved_event.for_sale_config.objects).is_empty()
         assert_that(retrieved_event.for_sale_config.area_places).is_equal_to({})
         assert_that(retrieved_event.for_sale_config.categories).is_empty()
+
 
 
